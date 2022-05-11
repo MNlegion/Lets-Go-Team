04e020fee6e6ed00c6a8d637cf41054832625c02
MNlegion <rkreuser30@gmail.com>
2022-05-11 17:23:39 -0500

    Inquirer prompt setup

--- index.js
+++ index.js
@@ -8,3 +8,60 @@ const Intern = require("./lib/Intern");
 const html = require("./src/htmlDisplay");
 const validator = require("email-validator");
 
+// Set up Async functions
+// const writeFileAsync = util.promisify(fs.writeFile);
+// const appendFileAsync = util.promisify(fs.appendFile);
+
+
+// Inquirer prompts for user command line input
+async function prompt() {
+    let responseDone = "";
+
+    do {
+        try {
+            console.log("---------------------------------------------");
+            let response = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is the employee's name?: ",
+                    validate: function validateName(name){
+                        return name !== '';
+                    }
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Enter the employee's ID: ",
+                    validate: function validateName(name){
+                        return name !== '';
+                    }
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Enter the employee's email address: ",
+                    //email-validator npm package
+                    validate: function validateEmail(name){
+                        return validator.validate(name);
+                    }
+                },
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What what is the employee's role:",
+                    choices: [
+                         "Engineer",
+                         "Intern",
+                         "Manager"
+                    ]
+                }
+            ]);
+
+        };
+    };
+};
+
+
+// Run Application
+// main();

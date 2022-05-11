2a407053846ac65ec7dc5cd92d55fc27af6f4997
MNlegion <rkreuser30@gmail.com>
2022-05-11 15:18:26 -0500

    SRC test HTML and initial index.js requirement setup

---
+++ index.js
@@ -0,0 +1,10 @@
+// Requirements
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./src/htmlDisplay");
+const validator = require("email-validator");
+

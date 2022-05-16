0323c477b1acd4c2572d5577dc0500bc9f914699
MNlegion <rkreuser30@gmail.com>
2022-05-15 22:03:57 -0500

    response2 capture for role specific questions

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const validator = require("email-validator");
 // const writeFileAsync = util.promisify(fs.writeFile);
 // const appendFileAsync = util.promisify(fs.appendFile);
 
+// team array and string for html and css
+let teamArray = [];
+let teamString = ``;
+
+// need main function for main call below to run application and generate html
+
 
 // Inquirer prompts for user command line input
 async function prompt() {
@@ -41,7 +47,7 @@ async function prompt() {
                     type: "input",
                     name: "email",
                     message: "Enter the employee's email address: ",
-                    //email-validator npm package
+                    // Validate email with email-validator
                     validate: function validateEmail(name){
                         return validator.validate(name);
                     }
@@ -58,10 +64,67 @@ async function prompt() {
                 }
             ]);
 
-        };
-    };
-};
+            let response2 = ""
 
+               if (response.role === "Engineer") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What is the employee's github username?:",
+                         validate: function validateName(name){
+                            return name !== '';
+                        },
+                    }, ]);
+
+                    // Add to team Array
+                    const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+                    teamArray.push(engineer);
+
+               } else if (response.role === "Intern") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What school is the employee attending?:",
+                         validate: function validateName(name){
+                            return name !== '';
+                        },
+                    }, ]);
+
+                    // Add to team Array
+                    const intern = new Intern(response.name, response.id, response.email, response2.x);
+                    teamArray.push(intern);
+
+               } else if (response.role === "Manager") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What is the employee's office number?:",
+                         validate: function validateName(name){
+                            return name !== '';
+                        },
+                    }, ]);
+
+                    // Add to team Array
+                    const manager = new Manager(response.name, response.id, response.email, response2.x);
+                    teamArray.push(manager);
+               }
+
+        } catch (err) {
+            return console.log(err);
+        }
+
+          responseDone = await inquirer.prompt([{
+               type: "list",
+               name: "finish",
+               message: "Do you want to continue?: ",
+               choices: [
+                    "Yes",
+                    "No"
+               ]
+            },]);
+
+     } while (responseDone.finish === "Yes");
+}
 
 // Run Application
-// main();
+main();
